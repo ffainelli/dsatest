ffainelli/dsatest

commit c6fd6d80d91c5be9a1cc7204c676a133f7121db2

bench: Support non standard protocol ports
Allow specifying non standard ports in a standard fashion by using:

protocol://host:port

Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>

bench/control.py
@@ -29,7 +29,7 @@ class Control(object):
 
 class LocalControl(Control):
 
-    def __init__(self, hostname, bench_parser):
+    def __init__(self, hostname, port, bench_parser):
         pass
 
     def exec(self, command):
@@ -55,7 +55,7 @@ class SSHControl(Control):
 
     SSH_TIMEOUT = 15
 
-    def __init__(self, address, bench_parser):
+    def __init__(self, address, port, bench_parser):
         # let's search extra argument we might need
         target_section = bench_parser.config[bench_parser.TARGET_IDENTIFIER]
 
@@ -77,6 +77,7 @@ class SSHControl(Control):
         logging.getLogger("paramiko").setLevel(logging.WARNING)
 
         self.address = address
+        self.port = port
         self.ssh_client = paramiko.SSHClient()
         self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ssh_client.load_system_host_keys(self.system_host_keys)
@@ -85,20 +86,20 @@ class SSHControl(Control):
         username, password, keyfile = self.strip_variables(
                 self.username, self.password, self.keyfile)
         if password is not None and keyfile is not None:
-            self.ssh_client.connect(self.address, username=username,
+            self.ssh_client.connect(self.address, self.port, username=username,
                                     key_filename=keyfile, password=password,
                                     timeout=SSHControl.SSH_TIMEOUT)
         elif keyfile is not None:
-            self.ssh_client.connect(self.address, username=username,
+            self.ssh_client.connect(self.address, self.port, username=username,
                                     key_filename=keyfile,
                                     timeout=SSHControl.SSH_TIMEOUT)
         elif password is not None:
-            self.ssh_client.connect(self.address, username=username,
+            self.ssh_client.connect(self.address, self.port, username=username,
                                     password=password, look_for_keys=False,
                                     timeout=SSHControl.SSH_TIMEOUT)
         else:
             # let's try with SSH agent, hopefully key will be not encrypted
-            self.ssh_client.connect(self.address, username=username,
+            self.ssh_client.connect(self.address, self.port, username=username,
                                     timeout=SSHControl.SSH_TIMEOUT)
 
     def disconnect(self):

bench/bench.py
@@ -25,10 +25,10 @@ class Bench:
         target_scheme = URI(bench_parser.target_control)
 
         host_ctrl = self.controls[host_scheme.getScheme(default_scheme)](
-                host_scheme.getHost(),
+                host_scheme.getHost(), host_scheme.getPort(),
                 bench_parser)
         target_ctrl = self.controls[target_scheme.getScheme(default_scheme)](
-                target_scheme.getHost(),
+                target_scheme.getHost(), target_scheme.getPort(),
                 bench_parser)
 
         # Create machines involved in the test bench

helper/scheme.py
@@ -12,18 +12,30 @@ class URI:
         "local":    Scheme.LOCAL,
     }
 
+    to_ports = {
+        "ssh":      22,
+        "local":    0,
+    }
+
     def __init__(self, uri):
         self.uri = uri
 
         if self.uri:
-            pattern = re.compile("^([a-z]+)://(.+)$")
+            pattern = re.compile("^([a-z]+)://([a-z0-9.]+)(:([\d]+))?$")
             match = pattern.match(self.uri)
             if match:
                 self.const = self.to_constants[match.group(1)]
                 self.host = match.group(2)
+                if match.group(4) is not None:
+                    self.port = int(match.group(4))
+                else:
+                    self.port = self.to_ports[match.group(1)]
 
     def getScheme(self, default=None):
         return getattr(self, "const", default)
 
     def getHost(self, default=None):
         return getattr(self, "host", default)
+
+    def getPort(self, default=None):
+        return getattr(self, "port", default)

commit 1fbc6f9ae5a3d7060328c4960f190569327a7660

bench: Fix passing non-standard port to telnet control
Non-standard ports would not be passed to the TelnetControl class,
leading to telnetlib using the standard telnet port.

Fixes: c6fd6d80d91c ("bench: Support non standard protocol ports")

bench/control.py
@@ -171,7 +171,7 @@ class TelnetControl(Control):
 
         self.address = address
         self.port = port
-        self.telnet_client = telnetlib.Telnet(address)
+        self.telnet_client = telnetlib.Telnet(address, port)
         self.prompt = self.prompt.replace('"', '')
 
 